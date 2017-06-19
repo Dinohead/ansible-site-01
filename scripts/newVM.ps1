c1aaee76bb4e98d13f4693e48f0c0e7be42f0f00
derek <derek@dinohead.net>
2017-06-19 16:50:40 -0500

    working on newUbuntuServer.yml

---
+++ scripts/newVM.ps1
@@ -0,0 +1,4 @@
+$VM = "testVM"
+$VMName = Get-VM -name $VM
+if (!$VMname) {
+New-VM -Name "testVM" -ComputerName data -MemoryStartupBytes 2GB -VHDPath "C:\ClusterStorage\cluster-storage-01\vm\testVM\Virtual Hard Disks\disk-0.vhdx" -Path "C:\ClusterStorage\cluster-storage-01\vm"
