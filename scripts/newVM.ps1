ba753b7f809e839c6f7a6128fffd60463876231b
derek <derek@dinohead.net>
2017-06-19 16:57:33 -0500

    working on newUbuntuServer.yml

--- scripts/newVM.ps1
+++ scripts/newVM.ps1
@@ -1,4 +1,4 @@
 $VM = "testVM"
-$VMName = Get-VM -name $VM
+$VMName = Get-VM -name $VM -erroraction 'ignore'
 if (!$VMname) {
 New-VM -Name "testVM" -ComputerName data -MemoryStartupBytes 2GB -VHDPath "C:\ClusterStorage\cluster-storage-01\vm\testVM\Virtual Hard Disks\disk-0.vhdx" -Path "C:\ClusterStorage\cluster-storage-01\vm"}
