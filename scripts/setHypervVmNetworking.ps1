ea1097023503095b85753ea830d0d115abbdf434
derek <derek@dinohead.net>
2017-06-20 09:36:01 -0500

    added dns entry and add to cluster

--- scripts/setHypervVmNetworking.ps1
+++ scripts/setHypervVmNetworking.ps1
@@ -1,85 +1,87 @@
-Function Set-VMNetworkConfiguration {
-     [CmdletBinding()]
-     Param (
-         [Parameter(Mandatory=$true,
-                    Position=1,
-                    ParameterSetName='DHCP',
-                    ValueFromPipeline=$true)]
-         [Parameter(Mandatory=$true,
-                    Position=0,
-                    ParameterSetName='Static',
-                    ValueFromPipeline=$true)]
-         [Microsoft.HyperV.PowerShell.VMNetworkAdapter]$NetworkAdapter,
+{
+    Function Set-VMNetworkConfiguration {
+         [CmdletBinding()]
+         Param (
+             [Parameter(Mandatory=$true,
+                        Position=1,
+                        ParameterSetName='DHCP',
+                        ValueFromPipeline=$true)]
+             [Parameter(Mandatory=$true,
+                        Position=0,
+                        ParameterSetName='Static',
+                        ValueFromPipeline=$true)]
+             [Microsoft.HyperV.PowerShell.VMNetworkAdapter]$NetworkAdapter,
 
-         [Parameter(Mandatory=$true,
-                    Position=1,
-                    ParameterSetName='Static')]
-         [String[]]$IPAddress=@(),
+             [Parameter(Mandatory=$true,
+                        Position=1,
+                        ParameterSetName='Static')]
+             [String[]]$IPAddress=@(),
 
-         [Parameter(Mandatory=$false,
-                    Position=2,
-                    ParameterSetName='Static')]
-         [String[]]$Subnet=@(),
+             [Parameter(Mandatory=$false,
+                        Position=2,
+                        ParameterSetName='Static')]
+             [String[]]$Subnet=@(),
 
-         [Parameter(Mandatory=$false,
-                    Position=3,
-                    ParameterSetName='Static')]
-         [String[]]$DefaultGateway = @(),
+             [Parameter(Mandatory=$false,
+                        Position=3,
+                        ParameterSetName='Static')]
+             [String[]]$DefaultGateway = @(),
 
-         [Parameter(Mandatory=$false,
-                    Position=4,
-                    ParameterSetName='Static')]
-         [String[]]$DNSServer = @(),
+             [Parameter(Mandatory=$false,
+                        Position=4,
+                        ParameterSetName='Static')]
+             [String[]]$DNSServer = @(),
 
-         [Parameter(Mandatory=$false,
-                    Position=0,
-                    ParameterSetName='DHCP')]
-         [Switch]$Dhcp
-     )
+             [Parameter(Mandatory=$false,
+                        Position=0,
+                        ParameterSetName='DHCP')]
+             [Switch]$Dhcp
+         )
 
-     $VM = Get-WmiObject -Namespace 'root\virtualization\v2' -Class 'Msvm_ComputerSystem' | Where-Object { $_.ElementName -eq $NetworkAdapter.VMName }
-     $VMSettings = $vm.GetRelated('Msvm_VirtualSystemSettingData') | Where-Object { $_.VirtualSystemType -eq 'Microsoft:Hyper-V:System:Realized' }
-     $VMNetAdapters = $VMSettings.GetRelated('Msvm_SyntheticEthernetPortSettingData')
+         $VM = Get-WmiObject -Namespace 'root\virtualization\v2' -Class 'Msvm_ComputerSystem' | Where-Object { $_.ElementName -eq $NetworkAdapter.VMName }
+         $VMSettings = $vm.GetRelated('Msvm_VirtualSystemSettingData') | Where-Object { $_.VirtualSystemType -eq 'Microsoft:Hyper-V:System:Realized' }
+         $VMNetAdapters = $VMSettings.GetRelated('Msvm_SyntheticEthernetPortSettingData')
 
-     $NetworkSettings = @()
-     foreach ($NetAdapter in $VMNetAdapters) {
-         if ($NetAdapter.Address -eq $NetworkAdapter.MacAddress) {
-             $NetworkSettings = $NetworkSettings + $NetAdapter.GetRelated("Msvm_GuestNetworkAdapterConfiguration")
+         $NetworkSettings = @()
+         foreach ($NetAdapter in $VMNetAdapters) {
+             if ($NetAdapter.Address -eq $NetworkAdapter.MacAddress) {
+                 $NetworkSettings = $NetworkSettings + $NetAdapter.GetRelated("Msvm_GuestNetworkAdapterConfiguration")
+             }
          }
-     }
 
-     $NetworkSettings[0].IPAddresses = $IPAddress
-     $NetworkSettings[0].Subnets = $Subnet
-     $NetworkSettings[0].DefaultGateways = $DefaultGateway
-     $NetworkSettings[0].DNSServers = $DNSServer
-     $NetworkSettings[0].ProtocolIFType = 4096
+         $NetworkSettings[0].IPAddresses = $IPAddress
+         $NetworkSettings[0].Subnets = $Subnet
+         $NetworkSettings[0].DefaultGateways = $DefaultGateway
+         $NetworkSettings[0].DNSServers = $DNSServer
+         $NetworkSettings[0].ProtocolIFType = 4096
 
-     if ($dhcp) {
-         $NetworkSettings[0].DHCPEnabled = $true
-     } else {
-         $NetworkSettings[0].DHCPEnabled = $false
-     }
+         if ($dhcp) {
+             $NetworkSettings[0].DHCPEnabled = $true
+         } else {
+             $NetworkSettings[0].DHCPEnabled = $false
+         }
 
-     $Service = Get-WmiObject -Class "Msvm_VirtualSystemManagementService" -Namespace "root\virtualization\v2"
-     $setIP = $Service.SetGuestNetworkAdapterConfiguration($VM, $NetworkSettings[0].GetText(1))
+         $Service = Get-WmiObject -Class "Msvm_VirtualSystemManagementService" -Namespace "root\virtualization\v2"
+         $setIP = $Service.SetGuestNetworkAdapterConfiguration($VM, $NetworkSettings[0].GetText(1))
 
-     if ($setip.ReturnValue -eq 4096) {
-         $job=[WMI]$setip.job
-
-         while ($job.JobState -eq 3 -or $job.JobState -eq 4) {
-             start-sleep 1
+         if ($setip.ReturnValue -eq 4096) {
              $job=[WMI]$setip.job
-         }
 
-         if ($job.JobState -eq 7) {
-             write-host "Success"
+             while ($job.JobState -eq 3 -or $job.JobState -eq 4) {
+                 start-sleep 1
+                 $job=[WMI]$setip.job
+             }
+
+             if ($job.JobState -eq 7) {
+                 write-host "Success"
+             }
+             else {
+                 $job.GetError()
+             }
+         } elseif($setip.ReturnValue -eq 0) {
+             Write-Host "Success"
          }
-         else {
-             $job.GetError()
-         }
-     } elseif($setip.ReturnValue -eq 0) {
-         Write-Host "Success"
      }
- }
 
- Get-VMNetworkAdapter -VMName testVM | Set-VMNetworkConfiguration -IPAddress 10.0.1.29 -Subnet 255.255.255.0 -DNSServer 10.0.1.2,10.0.1.22,10.0.1.222 -DefaultGateway 10.0.1.1
+    Get-VMNetworkAdapter -VMName testVM | Set-VMNetworkConfiguration -IPAddress 10.0.1.29 -Subnet 255.255.255.0 -DNSServer 10.0.1.2,10.0.1.22,10.0.1.222 -DefaultGateway 10.0.1.1
+}
